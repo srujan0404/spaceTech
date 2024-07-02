d02fcd645e0afe8ab97d88c50c959c967a58359f
Srujan Dharma <140909375+srujan0404@users.noreply.github.com>
2024-07-02 15:04:54 +0530

    Update pix2pix.py

--- pix2pix.py
+++ pix2pix.py
@@ -47,3 +47,44 @@ def eo_image(file_path):
         img = np.dstack((b1, b2, b3))
     img = tf.cast(img, tf.float32)
     return img
+
+BUFFER_SIZE = 10000
+# The batch size of 1 produced better results for the U-Net in the original pix2pix experiment
+BATCH_SIZE = 1
+# Each image is 256x256 in size
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
+
+
+def resize(input_image, real_image, height, width):
+    input_image = tf.image.resize(input_image, [height, width],
+                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    real_image = tf.image.resize(real_image, [height, width],
+                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+    return input_image, real_image
+
+def random_crop(input_image, real_image):
+    stacked_image = tf.stack([input_image, real_image], axis=0)
+    cropped_image = tf.image.random_crop(
+      stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
+
+    return cropped_image[0], cropped_image[1]
+
+# Normalizing the images to [-1, 1]
+def normalize(input_image, real_image):
+    input_image = (input_image / 127.5) - 1
+    real_image = (real_image / 127.5) - 1
+
+    return input_image, real_image
+
+@tf.function()
+def random_jitter(input_image, real_image):
+    input_image, real_image = resize(input_image, real_image, 286, 286)
+    input_image, real_image = random_crop(input_image, real_image)
+
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.flip_left_right(input_image)
+        real_image = tf.image.flip_left_right(real_image)
+
+    return input_image, real_image
