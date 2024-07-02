31cf72dd4449e6b48f5a33e37ab15cec3d4e81c2
Srujan Dharma <140909375+srujan0404@users.noreply.github.com>
2024-07-02 19:58:56 +0530

    Update pix2pix.py

--- pix2pix.py
+++ pix2pix.py
@@ -110,3 +110,29 @@ def downsample(filters, size, apply_batchnorm=True):
 down_model = downsample(3, 4)
 down_result = down_model(tf.expand_dims(inp, 0))
 print (down_result.shape)
+
+
+def upsample(filters, size, apply_dropout=False):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+    tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                    padding='same',
+                                    kernel_initializer=initializer,
+                                    use_bias=False))
+
+  result.add(tf.keras.layers.BatchNormalization())
+
+  if apply_dropout:
+      result.add(tf.keras.layers.Dropout(0.5))
+
+  result.add(tf.keras.layers.ReLU())
+
+  return result
+
+
+up_model = upsample(3, 4)
+up_result = up_model(down_result)
+print (up_result.shape)
+
