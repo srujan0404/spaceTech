f7a87d45a546661813bf4ccd78e4e138d0cbd40a
Srujan Dharma <140909375+srujan0404@users.noreply.github.com>
2024-07-03 09:10:24 +0530

    training

--- pix2pix.py
+++ pix2pix.py
@@ -273,3 +273,45 @@ def generate_images(model, test_input, tar):
 
 for example_input, example_target in train_dataset.take(1):
   generate_images(generator, example_input, example_target)
+
+log_dir="logs/"
+
+summary_writer = tf.summary.create_file_writer(
+  log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+
+@tf.function
+def train_step(input_image, target, step):
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        gen_output = generator(input_image, training=True)
+
+        disc_real_output = discriminator([input_image, target], training=True)
+        disc_generated_output = discriminator([input_image, gen_output], training=True)
+
+        gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_output, target)
+        disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+
+    generator_gradients = gen_tape.gradient(gen_total_loss,
+                                          generator.trainable_variables)
+    discriminator_gradients = disc_tape.gradient(disc_loss,
+                                               discriminator.trainable_variables)
+
+    generator_optimizer.apply_gradients(zip(generator_gradients,
+                                          generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
+                                              discriminator.trainable_variables))
+
+    with summary_writer.as_default():
+      tf.summary.scalar('gen_total_loss', gen_total_loss, step=step//1000)
+      tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=step//1000)
+      tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=step//1000)
+      tf.summary.scalar('disc_loss', disc_loss, step=step//1000)
+
+
+    return {
+    'gen_total_loss': gen_total_loss,
+    'gen_gan_loss': gen_gan_loss,
+    'gen_l1_loss': gen_l1_loss,
+    'disc_loss': disc_loss,
+    'step_divided_by_1000': step // 1000  # Assuming you want to include this transformation as well
+    }
